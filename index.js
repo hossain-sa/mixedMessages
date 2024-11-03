c9c510f7aaa3c9e5352f7a792104b3dabd36a1cf
hossain-sa <hossain.syedarif87@gmail.com>
2024-11-03 23:53:10 +0000

    message randomized and displayed

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // quotes from https://blog.wonderbly.com/occasions/astrology-quotes/
-const astrologyMsgs = ["Stars impel but they don’t compel", "One’s astrological horoscope may be compared metaphorically to one’s genetic heritage; it cannot be changed, but we can do what we may with what we are given.", "When we pass judgment, we hit a dead end. When we analyze something with an open mind, we can explore a concept into infinity."];
+const astrologyMsgs = [
+  "Stars impel but they don’t compel",
+  "One’s astrological horoscope may be compared metaphorically to one’s genetic heritage; it cannot be changed, but we can do what we may with what we are given.",
+  "When we pass judgment, we hit a dead end. When we analyze something with an open mind, we can explore a concept into infinity.",
+];
 
 // quotes from https://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/
-const inspiringMsgs = ["Whatever the mind of man can conceive and believe, it can achieve.", "You miss 100% of the shots you don’t take.", "Life is 10% what happens to me and 90% of how I react to it."];
+const inspiringMsgs = [
+  "Whatever the mind of man can conceive and believe, it can achieve.",
+  "You miss 100% of the shots you don’t take.",
+  "Life is 10% what happens to me and 90% of how I react to it.",
+];
 
 // quotes from https://bookroo.com/quotes/nonsense
-const nonsenseMsgs = ["The fault lies not with the mob, who demands nonsense, but with those who do not know how to produce anything else.", "The very nature of Joy makes nonsense of our common distinction between having and wanting.", "In nonsense is strength."];
+const nonsenseMsgs = [
+  "The fault lies not with the mob, who demands nonsense, but with those who do not know how to produce anything else.",
+  "The very nature of Joy makes nonsense of our common distinction between having and wanting.",
+  "In nonsense is strength.",
+];
 
 // get random number between 1-3 to select msg category
 const categorySelector = Math.round(Math.random() * 2) + 1;
@@ -13,4 +25,18 @@ const categorySelector = Math.round(Math.random() * 2) + 1;
 // get random number between 1-3 for message selector
 const messageSelector = Math.round(Math.random() * 2) + 1;
 
-console.log(categorySelector, messageSelector);
+// display message function
+
+const displayMessage = () => {
+  switch (categorySelector) {
+    case 1:
+      return astrologyMsgs[messageSelector];
+    case 2:
+      return inspiringMsgs[messageSelector];
+    default:
+      return nonsenseMsgs[messageSelector];
+  }
+};
+
+// console.log(categorySelector, messageSelector);
+console.log(displayMessage());
